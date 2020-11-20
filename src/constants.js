6d69027509a963b9172a4022cdf02381e0478189
abhishekdey <abhishekdey@users.noreply.github.com>
2020-11-20 20:27:29 +0530

    File added

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const VISIBILITY_FILTERS = {
+    ALL: 'all',
+    COMPLETED: 'completed',
+    INCOMPLETE: 'incomplete'
+}
+
+export const userDetails = 'https://jsonplaceholder.typicode.com/users';
